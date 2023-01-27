d8ef3819599f19f84c91eb6ce38968e866d55d63
nagata-ichiko <nagataichiko2249@gmail.com>
2023-01-27 22:07:10 +0900

    jest設定

--- nextjs-gihyo-book/nextjs-gihyo-book/next.config.js
+++ nextjs-gihyo-book/nextjs-gihyo-book/next.config.js
@@ -1,10 +1,22 @@
-/** @type {import('next').NextConfig} */
+/** @type {import('next'). NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  compiler: {
-    styledComponents: true,
-  },
-};
+  compiler: (() => {
+    let compilerConfig = {
+      // styledComponents の 有効 化
+      styledComponents: true,
+    };
 
+    if (process.env.NODE_ENV === "production") {
+      compilerConfig = {
+        ...compilerConfig,
+        // 本番 環境 では React Testing Library で 使用 する data-testid 属性 を 削除
+        reactRemoveProperties: {
+          properties: ["^data-testid $"],
+        },
+      };
+    }
+    return compilerConfig;
+  })(),
+};
 module.exports = nextConfig;
